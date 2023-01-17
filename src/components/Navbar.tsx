c161db86ad9809ba510f613b4221e36be97b18a7
Avi <avishkar.yt@gmail.com>
2023-01-17 22:24:06 +0530

    initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='w-full bg-gray-800 text-white h-16 flex justify-center items-center px-5'>Navbar</div>
+  )
+}
+
+export default Navbar
