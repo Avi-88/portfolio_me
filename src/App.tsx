1f3a5868702c3a263c2f30ed726c4b1248fc68a0
Avi <avishkar.yt@gmail.com>
2023-01-18 01:57:33 +0530

    navbar initial design

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Navbar from './components/Navbar'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [darkMode, setSetDarkMode] = useState(true);
+
+  const handleMode = () => {
+    if (localStorage.getItem("color-theme")) {
+      if (localStorage.getItem("color-theme") === "light") {
+        setSetDarkMode(true);
+        document.documentElement.classList.add('dark');
+        localStorage.setItem("color-theme", "dark");
+      } else {
+        setSetDarkMode(false);
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem("color-theme", "light");
+      }
+    } else {
+      if (darkMode) {
+        setSetDarkMode(false);
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem("color-theme", "light");
+      } else {
+        setSetDarkMode(true);
+        document.documentElement.classList.add('dark');
+        localStorage.setItem("color-theme", "dark");
+      }
+    }
+  };
 
   return (
-    <div className='w-full '>
-        <Navbar/>
+    <div className="w-full overflow-x-hidden bg-gradient-to-b from-teal-50 via-white to-teal-50 dark:bg-gradient-to-b dark:from-[#0D0E12] dark:via-[#0D0E12] dark:to-[#0D0E12] min-h-screen">
+      <Navbar mode={darkMode} toggle={handleMode} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
-
-function Navbar() {
-  return (
-    <div className='w-full bg-gray-800 text-white h-16 flex justify-center items-center px-5'>Navbar</div>
-  )
+import React from "react";
+interface NavbarProps {
+  mode: boolean;
+  toggle: () => void
 }
 
-export default Navbar
+function Navbar({mode , toggle}:NavbarProps) {
+  return (
+    <div className="w-full text-gray-500  dark:text-[#e3e5e885] h-16 flex justify-between  items-center lg:px-32 md:px-20 px-10 py-10">
+      <div>
+        <img src="/vite.svg" className="h-10" />
+      </div>
+      <div className="flex justify-evenly items-center">
+        <div className="flex justify-evenly items-center  ">
+          <div className="dark:hover:bg-[#22262F] dark:hover:text-white border-white border cursor-pointer hover:border-gray-500 dark:border-transparent  rounded-full px-4 py-1">
+            <a className="py-2 px-0.5 text-xs sm:text-sm ">About</a>
+          </div>
+          <div className="dark:hover:bg-[#22262F] dark:hover:text-white border-white border cursor-pointer hover:border-gray-500 dark:border-transparent rounded-full px-4 py-1">
+            <a className="py-2 px-0.5 text-xs sm:text-sm ">Skills</a>
+          </div>
+          <div className="dark:hover:bg-[#22262F] dark:hover:text-white border-white border cursor-pointer hover:border-gray-500 dark:border-transparent rounded-full px-4 py-1">
+            <a className="py-2 px-0.5 text-xs sm:text-sm ">Projects</a>
+          </div>
+          <div className="dark:hover:bg-[#22262F] dark:hover:text-white border-white border cursor-pointer hover:border-gray-500 dark:border-transparent  rounded-full px-4 py-1">
+            <a className="py-2 px-0.5 text-xs sm:text-sm ">Contact</a>
+          </div>
+
+        </div>
+        <div className="relative inline-block w-10 ml-10  mr-2 align-middle select-none transition duration-200 ease-in">
+          <input
+            type="checkbox"
+            name="toggle"
+            id="toggle"
+            checked={mode}
+            onClick={() => toggle()}
+            className="bg-yellow-300 border-yellow-500 mr-1 focus:ring-transparent toggle-checkbox absolute top-1/2 -translate-y-1/2 block w-4 h-4 rounded-full border-2 appearance-none cursor-pointer"
+          />
+          <label
+            htmlFor="toggle"
+            className="toggle-label block h-6 -ml-1  rounded-full bg-transparent border-gray-500 border dark:border-0 cursor-pointer"
+          ></label>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
